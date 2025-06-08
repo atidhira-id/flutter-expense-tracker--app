6831901191df853a6f0386e74e5057b25a148bd5
Atidhira Habibillah <atidhiraid68@gmail.com>
2025-06-08 23:44:41 +0700

    feat: add widget for showing individual expense data

---
+++ lib/widgets/expenses_list/expense_item.dart
@@ -0,0 +1,42 @@
+import 'package:flutter/material.dart';
+import 'package:expense_tracker/models/expense.dart';
+
+class ExpenseItem extends StatelessWidget {
+  const ExpenseItem(this.expense, {super.key});
+
+  final Expense expense;
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      elevation: 0,
+      shape: RoundedRectangleBorder(
+        side: BorderSide(color: Colors.grey, width: 1),
+        borderRadius: BorderRadius.circular(8),
+      ),
+      child: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 16),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text(expense.title),
+                SizedBox(height: 8),
+                Row(
+                  children: [
+                    Icon(expense.categoryIcon),
+                    SizedBox(width: 8),
+                    Text(expense.formattedDate),
+                  ],
+                ),
+              ],
+            ),
+            Text('\$${expense.amount.toStringAsFixed(2)}'),
+          ],
+        ),
+      ),
+    );
+  }
+}
