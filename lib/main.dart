baf27769600f4e7278e5e78e68b2794104164bb5
Atidhira Habibillah <atidhiraid68@gmail.com>
2025-06-08 23:40:03 +0700

    feat: add title and get rid of debugBanner

--- lib/main.dart
+++ lib/main.dart
@@ -2,5 +2,11 @@ import 'package:flutter/material.dart';
 import 'package:expense_tracker/expenses.dart';
 
 void main() {
-  runApp(const MaterialApp(home: Expenses()));
+  runApp(
+    const MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Expense Tracker App',
+      home: Expenses(),
+    ),
+  );
 }
