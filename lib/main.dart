3269f23f6730ba99dc695ec078357b81b504e0f5
Atidhira Habibillah <atidhiraid68@gmail.com>
2025-06-10 06:10:55 +0700

    feat: add theme for lightmode

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,34 @@
 import 'package:flutter/material.dart';
 import 'package:expense_tracker/expenses.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+var kColorScheme = ColorScheme.fromSeed(
+  seedColor: const Color.fromARGB(1, 44, 189, 187),
+);
 
 void main() {
+  WidgetsFlutterBinding.ensureInitialized();
   runApp(
-    const MaterialApp(
+    MaterialApp(
       debugShowCheckedModeBanner: false,
       title: 'Expense Tracker App',
+      theme: ThemeData().copyWith(
+        colorScheme: kColorScheme,
+        appBarTheme: AppBarTheme().copyWith(
+          backgroundColor: kColorScheme.onPrimaryContainer,
+          foregroundColor: kColorScheme.primaryContainer,
+        ),
+        cardTheme: CardThemeData().copyWith(
+          color: kColorScheme.secondaryContainer,
+          margin: EdgeInsets.all(0),
+        ),
+        elevatedButtonTheme: ElevatedButtonThemeData(
+          style: ElevatedButton.styleFrom(
+            backgroundColor: kColorScheme.primaryContainer,
+          ),
+        ),
+        textTheme: GoogleFonts.poppinsTextTheme().copyWith(),
+      ),
       home: Expenses(),
     ),
   );
