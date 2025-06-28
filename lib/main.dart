41efc9bfda9ec33098f541af6770662a83e0b626
Atidhira Habibillah <atidhiraid68@gmail.com>
2025-06-28 22:28:03 +0700

    feat: change color scheme and create buildTheme function

--- lib/main.dart
+++ lib/main.dart
@@ -2,14 +2,41 @@ import 'package:flutter/material.dart';
 import 'package:expense_tracker/expenses.dart';
 import 'package:google_fonts/google_fonts.dart';
 
-var kColorScheme = ColorScheme.fromSeed(
-  seedColor: const Color.fromARGB(1, 44, 189, 187),
-);
+var kColorScheme = ColorScheme.fromSeed(seedColor: const Color(0xFF607D8B));
 
 var kDarkColorScheme = ColorScheme.fromSeed(
   brightness: Brightness.dark,
-  seedColor: Color.fromARGB(1, 17, 25, 24),
+  seedColor: Color(0xFF90A4AE),
 );
+
+ThemeData buildTheme(ColorScheme colorScheme) {
+  return ThemeData(
+    colorScheme: colorScheme,
+    scaffoldBackgroundColor: colorScheme.surface,
+    appBarTheme: AppBarTheme().copyWith(
+      backgroundColor: colorScheme.surface,
+      foregroundColor: colorScheme.onSurface,
+    ),
+    cardTheme: CardThemeData().copyWith(
+      color: colorScheme.primaryContainer,
+      margin: EdgeInsets.all(0),
+      shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(4)),
+    ),
+    elevatedButtonTheme: ElevatedButtonThemeData(
+      style: ElevatedButton.styleFrom(
+        backgroundColor: colorScheme.primaryContainer,
+      ),
+    ),
+    textTheme: GoogleFonts.poppinsTextTheme().copyWith(
+      bodyLarge: GoogleFonts.poppins(color: colorScheme.onSurface),
+      bodySmall: GoogleFonts.poppins(color: colorScheme.onSurface),
+      titleLarge: GoogleFonts.poppins(
+        color: colorScheme.onPrimaryContainer,
+        fontWeight: FontWeight.bold,
+      ),
+    ),
+  );
+}
 
 void main() {
   WidgetsFlutterBinding.ensureInitialized();
@@ -17,66 +44,8 @@ void main() {
     MaterialApp(
       debugShowCheckedModeBanner: false,
       title: 'Expense Tracker App',
-      darkTheme: ThemeData.dark().copyWith(
-        colorScheme: kDarkColorScheme,
-        appBarTheme: AppBarTheme().copyWith(
-          backgroundColor: kDarkColorScheme.primaryContainer,
-          foregroundColor: kDarkColorScheme.onPrimaryContainer,
-        ),
-        cardTheme: CardThemeData().copyWith(
-          color: kDarkColorScheme.secondaryContainer,
-          margin: EdgeInsets.all(0),
-          shape: RoundedRectangleBorder(
-            side: BorderSide(
-              color: kDarkColorScheme.secondaryContainer,
-              width: 1,
-            ),
-            borderRadius: BorderRadius.circular(4),
-          ),
-        ),
-        elevatedButtonTheme: ElevatedButtonThemeData(
-          style: ElevatedButton.styleFrom(
-            backgroundColor: kDarkColorScheme.primaryContainer,
-            foregroundColor: kDarkColorScheme.onPrimaryContainer,
-          ),
-        ),
-        textTheme: GoogleFonts.poppinsTextTheme().copyWith(
-          bodyLarge: GoogleFonts.poppins(color: kDarkColorScheme.onSurface),
-          bodySmall: GoogleFonts.poppins(color: kDarkColorScheme.onSurface),
-          titleLarge: GoogleFonts.poppins(
-            color: kDarkColorScheme.onPrimaryContainer,
-            fontWeight: FontWeight.bold,
-          ),
-        ),
-      ),
-      theme: ThemeData().copyWith(
-        colorScheme: kColorScheme,
-        appBarTheme: AppBarTheme().copyWith(
-          backgroundColor: kColorScheme.onPrimaryContainer,
-          foregroundColor: kColorScheme.primaryContainer,
-        ),
-        cardTheme: CardThemeData().copyWith(
-          color: kColorScheme.secondaryContainer,
-          margin: EdgeInsets.all(0),
-          shape: RoundedRectangleBorder(
-            side: BorderSide(color: kColorScheme.secondaryContainer, width: 1),
-            borderRadius: BorderRadius.circular(4),
-          ),
-        ),
-        elevatedButtonTheme: ElevatedButtonThemeData(
-          style: ElevatedButton.styleFrom(
-            backgroundColor: kColorScheme.primaryContainer,
-          ),
-        ),
-        textTheme: GoogleFonts.poppinsTextTheme().copyWith(
-          bodyLarge: GoogleFonts.poppins(color: kColorScheme.onSurface),
-          bodySmall: GoogleFonts.poppins(color: kColorScheme.onSurface),
-          titleLarge: GoogleFonts.poppins(
-            color: kColorScheme.onPrimaryContainer,
-            fontWeight: FontWeight.bold,
-          ),
-        ),
-      ),
+      darkTheme: buildTheme(kDarkColorScheme),
+      theme: buildTheme(kColorScheme),
       themeMode: ThemeMode.dark,
       home: Expenses(),
     ),
